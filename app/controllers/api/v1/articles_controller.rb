b8358b8636b0a2c0cd9a67af488a2a8a22285165
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-04-29 23:55:00 -0400

    added before action to articles controller for show, update and destroy actions

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,9 @@
 module Api
     module V1
         class ArticlesController < ApplicationController
+            before_action :find_article, only: [:update, :show, :destroy]
+
+
             def index
                 articles = Article.order('created_at DESC')
                 render json: {message: 'articles loaded', data: articles}, status: :ok
@@ -18,25 +21,22 @@ module Api
             end
             
             def update
-                article = Article.find(params[:id])
                 
-                article.update(article_params)
-                if article.valid?
-                    render json: {status: 'article updated', data: article}, status: :ok
+                @article.update(article_params)
+                if @article.valid?
+                    render json: {status: 'article updated', data: @article}, status: :ok
                 else
-                    render json: {status: 'error', message: 'article not updated', data: article.errors}, status: :unprocessable_entity
+                    render json: {status: 'error', message: 'article not updated', data: @article.errors}, status: :unprocessable_entity
                 end
                 
             end
             
             def show
-                article = Article.find(params[:id])
-                render json: {message: 'article requested', data: article}, status: :ok
+                render json: {message: 'article requested', data: @article}, status: :ok
             end
             
             def destroy
-                article = Article.find(params[:id])
-                article.destroy
+                @article.destroy
                 render json: {status: 'article deleted', data: article}, status: :ok 
             end
 
@@ -44,6 +44,10 @@ module Api
 
             def article_params
                 params.permit(:title, :body)
+            end
+
+            def find_article
+                @article = Article.find(params[:id]) 
             end
             
         end
